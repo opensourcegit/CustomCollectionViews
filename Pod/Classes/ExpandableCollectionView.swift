8d803f8aa564d9a75ab08a6c069f9e65c6eabc82
vikas <vikas4goyal@gmail.com>
2015-09-20 01:55:43 +0530

    code clean

--- Pod/Classes/ExpandableCollectionView.swift
+++ Pod/Classes/ExpandableCollectionView.swift
@@ -10,32 +10,18 @@ import Foundation
 import UIKit
 
 public class ExpandableCollectionView: BaseCollectionView,UIGestureRecognizerDelegate,ExpandableCollectionSectionCallBack {
-    private var tabGestureRecognizer:UITapGestureRecognizer?;
+
     override public func setCollectionViewData(data: [BaseCollectionSectionModel]) {
         super.setCollectionViewData(data);
     }
     override func setup() {
         super.setup();
-       // let gestureRecognizer = getGestureRecognizer();
-        //addGestureRecognizer(gestureRecognizer);
-        //self.tabGestureRecognizer = gestureRecognizer;
-        print("")
-    }
-    func getGestureRecognizer()->UITapGestureRecognizer{
-        if let tabGesture = self.tabGestureRecognizer{
-            return tabGesture;
-        }
-        let selector:Selector = "handleTapGestuer:";
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: selector);
-        gestureRecognizer.numberOfTapsRequired = 1;
-        gestureRecognizer.delegate = self;
-        return gestureRecognizer;
     }
 
     func indexPathsForSection(section:Int)->[NSIndexPath]{
         var indexpaths = [NSIndexPath]();
         let sectionModel = data[section] as! ExpandableCollectionSectionModel;
-        var count = sectionModel.numberOfitems() ;//self.dataSource?.collectionView(self, numberOfItemsInSection: section);
+        var count = sectionModel.numberOfitems() ;
         count = count-1;
         for i in 0...count{
             indexpaths.append(NSIndexPath(forItem: i, inSection: section));
@@ -44,22 +30,16 @@ public class ExpandableCollectionView: BaseCollectionView,UIGestureRecognizerDel
     }
     
 public override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
-        let view = super.collectionView(collectionView, viewForSupplementaryElementOfKind: kind, atIndexPath: indexPath) as! ExpandableCollectionSection;
+        let view = super.collectionView(collectionView, viewForSupplementaryElementOfKind: kind, atIndexPath: indexPath);
         view.tag = indexPath.section;
+    
+    if let view = view as? ExpandableCollectionSection{
         view.sectionDelegate = self;
+    }
         return view;
     }
-//MARK: gestureRecognizer delegates
-   public func  gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldReceiveTouch touch: UITouch) -> Bool {
-        if(gestureRecognizer === self.tabGestureRecognizer){
-            if(gestureRecognizer.state == UIGestureRecognizerState.Possible){
-                return true;
-            }
-            return false;
-        }
-        return true;
-    }
-   //ExpandableCollectionSectionCallBackDelegate
+
+   //MARK: ExpandableCollectionSectionCallBackDelegate
     func onClick(sectionView: ExpandableCollectionSection) {
         let viewK = sectionView;
         let tappedSection = viewK.tag;//tappedCellPath.section;
@@ -68,7 +48,6 @@ public override func collectionView(collectionView: UICollectionView, viewForSup
         let indexpaths = self.indexPathsForSection(tappedSection);
         self.performBatchUpdates({ () -> Void in
             if(willOpen){
-                //                self.deleteItemsAtIndexPaths(self.)
                 self.insertItemsAtIndexPaths(indexpaths);
             }else{
                 self.deleteItemsAtIndexPaths(indexpaths);
